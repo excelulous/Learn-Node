c29a0b2094436ab1f04f79c83a177326d1dd01da
Harry Nicholls <harry@northstar-robotics.com>
2018-05-22 17:37:42 -0500

    All additions up to video 21

--- delicioso/routes/index.js
+++ delicioso/routes/index.js
@@ -1,9 +1,42 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
-});
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
+router.get('/add', storeController.addStore);
+
+router.post('/add',
+    storeController.upload,
+    catchErrors(storeController.resize),
+    catchErrors(storeController.createStore)
+);
+
+router.post('/add/:id',
+    storeController.upload,
+    catchErrors(storeController.resize),
+    catchErrors(storeController.updateStore)
+);
+
+router.get('/stores/:id/edit', catchErrors(storeController.editStore))
+
+router.get('/login', userController.loginForm);
+router.get('/register', userController.registerForm);
+
+// Validate registration data
+// Register user
+// Login user
+router.post('/register', 
+    userController.validateRegister,
+    userController.register
+);
+
+router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
+
+router.get('/tags', catchErrors(storeController.getStoreByTag));
+router.get('/tags/:tag', catchErrors(storeController.getStoreByTag));
 
 module.exports = router;
